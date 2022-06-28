f2218de0b640b7ce53dd29e199d6a3c9bdd98f1f
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-28 13:40:54 +0300

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+      if (symbol > 0x0040 & symbol <= 0x007f) {
+          if (symbol == 0x0061 || symbol == 0x0065 ||
+          symbol == 0x0069 || symbol == 0x006f ||
+          symbol == 0x0075 || symbol == 0x0079 ) {
+          System.out.println("Vowel");}
+          else {
+              System.out.println("Consonant");}
+      } else {
+          System.out.println("wrong alphabet!");
+      } tem.out.println("Non English");
+      } 
     }
 }
