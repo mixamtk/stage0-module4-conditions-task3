344ee2dcbb657d94743f3c955e8f3526ec6ad0d4
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-28 11:39:52 +0300

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
-    public void isEnglishSymbol(char symbol) {
+    public void isEnglishSymbol(char symbol) {      
+      if (symbol > 0x0040 & symbol <= 0x007f) {
+          System.out.println("English");
+      } else {
+          System.out.println("Non English");
+      } 
     }
 }
